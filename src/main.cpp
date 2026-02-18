ca5f90cab63c87a250c48e3a5c77dbb4ff3e3464
NathanMelegari <melegarinathan43@gmail.com>
2026-02-17 23:35:59 -0300

    fix download errors

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,21 @@
 #include "../include/request.hpp"
 #include "../include/stats.hpp"
 
+void showHelp() {
+	std::cout << R"(
+	Lamon - API Monitor CLI
+
+	Usage:
+		lamon [URL]
+	
+	Options:
+		-h, --help	Show this help message
+
+	Examples:
+		lamon https://api.github.com
+	)";
+};
+
 void restore_cursor(int) {
 	std::cout << "\033[?25h";
 	std::cout.flush();
@@ -24,12 +39,16 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	/*
-	if(arg == "--help") {
-		std::cout << "Usage: lamon <url> | for monitoring\n";
-		std::cout << "Usage: lamon --help | for commands\n";
-		return 1;
-	};
-*/
+	if (argc > 1 && std::string(argv[1]) == "--version") {
+		std::cout << "lamon 0.1.0\n";
+		return 0;
+	}
+	*/
+	if(argc == 1 || std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h") {
+		showHelp();
+		return 0;
+	}
+
 	const char* url = argv[1];
 	Stats stats;
 
